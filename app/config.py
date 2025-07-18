9c401fdd417d18ba9abbeb5c39b65fd9e6a20f7d
LirSkara <itcp199@gmail.com>
2025-07-18 19:10:07 +0300

    Стабильная стадия

--- app/config.py
+++ app/config.py
@@ -53,12 +53,15 @@ class Settings(BaseSettings):
     # CORS - Конфигурируем origins в зависимости от окружения
     cors_origins: List[str] = [
         "http://localhost:5173", 
-        "http://127.0.0.1:5173", 
+        "http://127.0.0.1:5173",
+        "http://localhost:3002", 
+        "http://127.0.0.1:3002",
+        "http://localhost:5174", 
+        "http://127.0.0.1:5174", 
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
         "http://localhost:8080",
         "http://127.0.0.1:8080"
-        # УБРАЛИ "*" - он не совместим с allow_credentials=True
     ]
     
     # Trusted Hosts - Разрешенные хосты для защиты от Host Header Injection
